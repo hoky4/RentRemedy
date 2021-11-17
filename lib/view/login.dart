cd40e1ce5ba316724130c5a277bf957c10d79404
kennyho94 <kennyho94@gmail.com>
2021-11-17 11:38:43 -0700

    - Fixed overflow issue for login - updated login button type from FlatButton to TextButton

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -21,142 +21,142 @@ class _LoginState extends State<Login> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(36.0),
-        child: Stack(
-          children: [
-            Container(
-                alignment: Alignment.center,
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(height: 100),
-                      Text(
-                        "Login",
-                        style: GoogleFonts.pacifico(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 50,
-                            color: Colors.black),
+        child: SingleChildScrollView(
+          child: Container(
+              alignment: Alignment.center,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 100),
+                    Text(
+                      "Login",
+                      style: GoogleFonts.pacifico(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 50,
+                          color: Colors.black),
+                    ),
+                    SizedBox(height: 25),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: TextEditingController(text: user.email),
+                        onChanged: (value) {
+                          user.email = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Email is required';
+                          } else if (RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                            return null;
+                          } else {
+                            return 'Enter valid email';
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.red)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.red)),
+                        ),
                       ),
-                      SizedBox(height: 25),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextFormField(
-                          controller: TextEditingController(text: user.email),
-                          onChanged: (value) {
-                            user.email = value;
-                          },
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Email is required';
-                            } else if (RegExp(
-                                    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                                .hasMatch(value)) {
-                              return null;
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: TextFormField(
+                        controller: TextEditingController(text: user.password),
+                        onChanged: (value) {
+                          user.password = value;
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Password is required';
+                          } else {
+                            return null;
+                          }
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter Password',
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.blue)),
+                          errorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.red)),
+                          focusedErrorBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16),
+                              borderSide: BorderSide(color: Colors.red)),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Container(
+                        height: 50,
+                        width: 400,
+                        child: TextButton(
+                          style: TextButton.styleFrom(
+                            padding: const EdgeInsets.all(8.0),
+                            primary: Colors.white,
+                            backgroundColor: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16.0)),
+                            textStyle: const TextStyle(fontSize: 20),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              print('ok');
                             } else {
-                              return 'Enter valid email';
+                              print('not ok');
                             }
                           },
-                          decoration: InputDecoration(
-                            hintText: 'Enter Email',
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.blue)),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.blue)),
-                            errorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.red)),
-                            focusedErrorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.red)),
+                          child: const Text('Login'),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(65, 20, 0, 0),
+                        child: Row(children: [
+                          Text(
+                            "Don't have Account ? ",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: TextFormField(
-                          controller:
-                              TextEditingController(text: user.password),
-                          onChanged: (value) {
-                            user.password = value;
-                          },
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Password is required';
-                            } else {
-                              return null;
-                            }
-                          },
-                          decoration: InputDecoration(
-                            hintText: 'Enter Password',
-                            enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.blue)),
-                            focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.blue)),
-                            errorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.red)),
-                            focusedErrorBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(16),
-                                borderSide: BorderSide(color: Colors.red)),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(16.0),
-                        child: Container(
-                          height: 50,
-                          width: 400,
-                          child: FlatButton(
-                              color: Colors.blue,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(16.0)),
-                              onPressed: () {
-                                if (_formKey.currentState!.validate()) {
-                                  print('ok');
-                                } else {
-                                  print('not ok');
-                                }
-                              },
-                              child: Text(
-                                'Signin',
-                                style: TextStyle(color: Colors.white),
-                              )),
-                        ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.fromLTRB(65, 20, 0, 0),
-                          child: Row(children: [
-                            Text(
-                              "Don't have Account ? ",
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (context) => Signup()));
+                            },
+                            child: Text(
+                              'Signup',
                               style: TextStyle(
-                                  color: Colors.black,
+                                  color: Colors.blue,
                                   fontWeight: FontWeight.bold),
                             ),
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    new MaterialPageRoute(
-                                        builder: (context) => Signup()));
-                              },
-                              child: Text(
-                                'Signup',
-                                style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ])),
-                    ],
-                  ),
-                ))
-          ],
+                          ),
+                        ])),
+                  ],
+                ),
+              )),
         ),
       ),
     );
